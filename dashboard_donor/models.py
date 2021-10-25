b58d1aefd5118555683a0d82041d961c80a69ec7
Romi Fadhurrohman Nabil <romi.fadhurrohman@gmail.com>
2021-10-25 14:51:48 +0700

    Adding models for dashboard_donor

--- dashboard_donor/models.py
+++ dashboard_donor/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from datetime import date
 
 # Create your models here.
+class Notifications(models.Model) :
+    title = models.CharField(max_length=50)
+    message = models.CharField(max_length=1000)
+    timestamp = models.DateTimeField(default=date.today)
+    status = models.CharField(max_length=20)
