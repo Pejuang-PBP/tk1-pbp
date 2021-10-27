b8163db38b8935287d68cdca78a39d2bf42af4c8
ana117 <lsaga219@gmail.com>
2021-10-27 22:54:52 +0700

    Add donor/recipient counter

--- landing_page/urls.py
+++ landing_page/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import index, signup_page, login_page, logout_request
+from .views import index, signup_page, login_page, get_count
 
 urlpatterns = [
   path('', index, name="home"),
   path('signup', signup_page, name="signup_page"),
   path('login', login_page, name="login_page"),
-  path('logout', logout_request, name="logout")
+  path('get_count', get_count)
 ]
--- landing_page/views.py
+++ landing_page/views.py
@@ -1,23 +1,29 @@
+from django.http.response import JsonResponse
 from django.shortcuts import render, redirect
 from django.conf import settings
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.forms import AuthenticationForm
-
+from django.contrib.auth.models import User
 from .models import Slide
 from .forms import NewUserForm
+from form_pencari_donor.models import request_pencari_donor
+from form_donor.models import request_donor
 
 # Create your views here.
 def index(request):
   slides = Slide.objects.all().values()
 
-  response = {"slides": slides, "media_url": settings.MEDIA_URL}
+  response = {
+    "slides": slides, 
+    "media_url": settings.MEDIA_URL
+  }
   return render(request, 'index.html', response)
 
 
 def signup_page(request):
   if request.method == "POST":
     form = NewUserForm(request.POST)
-    
+
     if form.is_valid():
       user = form.save()
       login(request, user)
@@ -48,6 +54,9 @@ def login_page(request):
   return render(request, "login_page.html")
 
 
-def logout_request(request):
-  logout(request)
-  return redirect("home")
+def get_count(request):
+  return JsonResponse({
+    "donor_count": request_donor.objects.count(),
+    "recipient_count": request_pencari_donor.objects.count(),
+    "user_count": User.objects.count()
+  }, status=200)
