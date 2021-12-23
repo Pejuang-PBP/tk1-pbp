0fb07cf34235b9665ead014402edeac7bea2efc0
Adillah Putri <adllhputri@gmail.com>
2021-12-23 01:40:26 +0000

    Added function and url in for mobile

--- form_donor/views.py
+++ form_donor/views.py
@@ -4,7 +4,10 @@ from .forms import request_donor_form
 from django.core import serializers
 from .models import request_donor
 from django.http import JsonResponse
-
+from django.views.decorators.csrf import csrf_exempt
+import json
+from datetime import datetime
+from django.views.decorators.http import require_http_methods
 
 @login_required(login_url='/login')
 def index(request):
@@ -22,3 +25,20 @@ def index(request):
         else:
             return render(request, "form_donor.html", {'form': request_donor_form()})
     return render(request, "fail.html")
+
+@csrf_exempt
+@require_http_methods(["POST"])
+def mobile(request):
+	if request.user.is_authenticated and not hasattr(request.user, 'request_donor'):
+		data = json.loads(request.body)
+		data['tanggal_lahir'] = datetime.strptime(data['tanggal_lahir'], '%Y-%m-%d %H:%M:%S.000')
+		form = request_donor_form(data);
+		if form.is_valid():
+			obj = form.instance
+			obj.user = request.user
+			obj.save();
+		return JsonResponse({'status': 'ok', 'msg': 'Your form has been submitted successfully'})
+	elif not request.user.is_authenticated:
+		return JsonResponse({'status': 'error', 'msg': 'Silakan login terlebih dahulu'})
+	else:
+		return JsonResponse({'status': 'error', 'msg': 'Request sudah pernah dibuat'})
