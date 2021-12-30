4ee064d6a58373027602cb4affed7f1a829a5410
adnan-mkhtr <adnanmuchtar8e01@gmail.com>
2021-12-30 11:11:04 +0700

    edited views.py

--- form_pencari_donor/views.py
+++ form_pencari_donor/views.py
@@ -3,7 +3,10 @@ from django.http.response import JsonResponse
 from django.shortcuts import render
 from .models import request_pencari_donor
 from form_pencari_donor.forms import request_pencari_donor_form
-
+from django.views.decorators.csrf import csrf_exempt
+import json
+from datetime import datetime
+from django.views.decorators.http import require_http_methods
 # Create your views here.
 
 
@@ -22,3 +25,21 @@ def index(request):
         else:
             return render(request, "form_pencari_donor.html", {'form': request_pencari_donor_form()})
     return render(request, "fail.html")
+
+@csrf_exempt
+@require_http_methods(["POST"])
+def mobile(request):
+    if request.user.is_authenticated and not hasattr(request.user, 'request_donor'):
+        data = json.loads(request.body)
+        data['tanggal_lahir'] = datetime.strptime(data['tanggal_lahir'], '%Y-%m-%d %H:%M:%S.000')
+        form = request_pencari_donor_form(data);
+        if form.is_valid():
+            obj = form.instance
+            obj.user = request.user
+            obj.save();
+            return JsonResponse({'status': 'ok', 'msg': 'Your form has been submitted successfully'})
+        return JsonResponse({'status': 'error', 'msg': form.errors})
+    elif not request.user.is_authenticated:
+        return JsonResponse({'status': 'error', 'msg': 'Silakan login terlebih dahulu'})
+    else:
+        return JsonResponse({'status': 'error', 'msg': 'Request sudah pernah dibuat'})
